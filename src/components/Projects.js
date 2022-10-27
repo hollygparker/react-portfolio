dac434801ac36c08ddbe7bdd5fc84cd46df189fd
Holly Parker <hollygcochran@gmail.com>
2022-10-27 14:47:01 -0500

    adding projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,34 +15,34 @@
 const projects = [
     {
       id: 1,
-      name: 'Project 1',
-      href: '#',
-      price: '$48',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
+      name: 'Good Weeds',
+      href: 'https://goodweeds.herokuapp.com/',
+      repo: 'github',
+      imageSrc: 'https://user-images.githubusercontent.com/67671637/192348054-1213ed5c-ea06-485d-a69b-8310388130ca.png',
       imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
     },
     {
       id: 2,
-      name: 'Nomad Tumbler',
+      name: 'INtertain',
       href: '#',
-      price: '$35',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
+      repo: 'github',
+      imageSrc: 'https://github.com/NW-Bootcamp-Team1/Night-In/raw/main/assets/Homepage.png',
       imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
     },
     {
       id: 3,
-      name: 'Focus Paper Refill',
-      href: '#',
-      price: '$89',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
-      imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
+      name: 'Work Day Scheduler',
+      href: 'https://hollygparker.github.io/hw5-WorkDayScheduler/',
+      repo: 'GITHUB',
+      imageSrc: 'https://user-images.githubusercontent.com/67671637/179055125-894563b3-48ab-4b6a-bd83-a999de753163.png',
+      imageAlt: 'Person using a pen to cross a task off a projectsivity paper card.',
     },
     {
       id: 4,
-      name: 'Machined Mechanical Pencil',
-      href: '#',
-      price: '$35',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+      name: 'J.A.T.E',
+      href: 'https://jate-text-editor-404.herokuapp.com/',
+      repo: 'https://github.com/hollygparker/PWA-TextEditor-JATE',
+      imageSrc: 'https://user-images.githubusercontent.com/67671637/197890701-91ede160-d31e-4e7a-974f-e00e6fa873ea.png',
       imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
     },
     // More projects...
@@ -55,17 +55,17 @@ const projects = [
           <h2 className="sr-only">Projects</h2>
   
           <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-            {projects.map((product) => (
-              <a key={product.id} href={product.href} className="group">
+            {projects.map((projects) => (
+              <a key={projects.id} href={projects.href} className="group">
                 <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
                   <img
-                    src={product.imageSrc}
-                    alt={product.imageAlt}
+                    src={projects.imageSrc}
+                    alt={projects.imageAlt}
                     className="h-full w-full object-cover object-center group-hover:opacity-75"
                   />
                 </div>
-                <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-                <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
+                <h3 className="mt-4 text-sm text-gray-700">{projects.name}</h3>
+                <a className="mt-1 text-lg font-medium text-gray-900">{projects.repo}</a>
               </a>
             ))}
           </div>
