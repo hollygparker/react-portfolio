60f00cdd1680aa6e2554cb3b1879e8d575110e22
Holly Parker <hollygcochran@gmail.com>
2022-11-03 10:45:11 -0500

    updating title

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ function About() {
   return (
     <div className="mt-10 sm:mt-20 ml-10 mr-10 bg-slate-100 p-5">
     <h1 className='text-2xl tracking-tight text-indigo-700 sm:text-3xl text-center'>Holly Parker</h1>
-    <h2 className='mt-7 text-lg'>As a passionate problem solver ..... I reside in Chicago area with my wife and two precious cats. I am looking to pursue an endevor that will allow me to oppt to grow my coding experience.</h2>
+    <h2 className='mt-7 text-lg'>As a passionate problem solver ..... I reside in Chicago area with my wife and two precious cats. I am looking to pursue an endeavor that will allow me an oppt to grow my coding experience.</h2>
     </div>
   )
 }
